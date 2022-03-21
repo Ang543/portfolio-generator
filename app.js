fbe84718e4efc469b136fb971c8d60955813118b
Angelo <angelocsaverino@gmail.com>
2022-03-20 23:30:31 -0400

    updated template

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template');
 
+// const mockData = {
+//   name: 'Lernantino',
+//   github: 'lernantino',
+//   projects: []
+// }
+
+// const pageHTML = generatePage(mockData);
+
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -129,11 +137,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
